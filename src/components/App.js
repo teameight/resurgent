1f954b8a244dca976a8fb0efed191ba96e8eef45
Andrew Mowe <andrew@team-eightcom>
2017-08-30 00:01:21 -0400

    build plus add new sample providers

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ import Auth from '../Auth/Auth';
 import styles from '../css/style.css';
 import AreaPicker from './AreaPicker';
 import ProviderPicker from './ProviderPicker';
-import sampleUsers from '../sample-users.js';
+import sampleProviders from '../sample-providers.js';
 import BookSession from './BookSession';
 import BookConfirm from './BookConfirm';
 import Rating from './Rating';
@@ -88,7 +88,7 @@ class App extends React.Component {
 
 	loadSamples() {
 		this.setState({
-			users: sampleUsers
+			providers: sampleProviders
 		});
 	}
 
@@ -110,7 +110,7 @@ class App extends React.Component {
 		if(this.props.location.pathname == '/edit-account'){
 			this.state.isModal = true;
 		}
-		
+
 		if(this.state.isModal){
 			wrapClassName += ' modal';
 		}
